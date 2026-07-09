976a713202c697cfb71626d85cfe7db3ce7687a6
SaveKiller <saveabb@gmail.com>
2026-07-09 16:37:27 +0200

    sync aggiornato

--- _test_sync_loop.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set HOST=ticksaver
-set REMOTE=/opt/btc5min/data
-for /f "delims=" %%D in ('ssh %HOST% "find %REMOTE% -maxdepth 1 -mindepth 1 -type d -regextype posix-extended -regex '.*/[0-9]{4}-[0-9]{2}-[0-9]{2}$' -printf '%%f\n'"') do echo %%D
